dc3c7bf7c15eff5e26e396d49f9cb6043e78b514
467980 <benedickcorpuz274@gmail.com>
2025-07-08 11:05:24 +0800

    Update and rename README.md to arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,16 @@
+//create a array of favorite singers
+let favSingers = ["Taylor Swift", "Ed Sheeran", "Adele"];
+
+//array index is 1 it choice ed sheeran
+console.log("First favorite singer:", favSingers[1]);
+
+//create a array of favorite numbers
+let favNumbers = [7, 11, 23, 42];
+
+//create mixed array
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log("First item:", mixedArr[0]);
+console.log("Second item:", mixedArr[1]);
+console.log("Third item:", mixedArr[2]);
+console.log("Fourth item:", mixedArr[3]);
