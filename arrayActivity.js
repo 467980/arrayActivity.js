c3b59520812a57600782930edc410ef3a8eb5de9
467980 <benedickcorpuz274@gmail.com>
2025-07-08 11:22:41 +0800

    Update arrayActivity.js

--- arrayActivity.js
+++ arrayActivity.js
@@ -1,13 +1,7 @@
-//create a array of favorite singers
 let favSingers = ["Taylor Swift", "Ed Sheeran", "Adele"];
-
-//array index is 1 it choice ed sheeran
 console.log("First favorite singer:", favSingers[1]);
 
-//create a array of favorite numbers
 let favNumbers = [7, 11, 23, 42];
-
-//create mixed array
 let mixedArr = ["string", ["otherarray"], 123, true];
 
 console.log("First item:", mixedArr[0]);
